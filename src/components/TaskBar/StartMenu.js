72280e438a2d405978ebe5253670c0cfa7f34c98
Trip1701 <bj2336@email.vccs.edu>
2019-01-19 13:57:19 -0500

    Nested Menu Implemented

--- src/components/TaskBar/StartMenu.js
+++ src/components/TaskBar/StartMenu.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import minesweeper from '../../images/minesweeper.png';
+
 const StartMenu = props => {
 
   const Menu = styled.div`
@@ -21,6 +23,8 @@ const StartMenu = props => {
     display: flex;
     align-items: center;
 
+    position: relative;
+
     p {
       margin-left: 15px;
     }
@@ -33,6 +37,16 @@ const StartMenu = props => {
     &:last-child {
       border-top: 1px inset #808080;
     }
+
+    &:first-child:after {
+      content: '\\25B6';
+      font-size: 1.2rem;
+      font-weight: bold;
+
+      position: absolute;
+      right: 25px;
+      bottom: 14.75px;
+    }
   `;
 
   let displayStatus;
@@ -41,16 +55,40 @@ const StartMenu = props => {
     displayStatus = 'hidden';
   }
 
+  const miniMenu = [
+    {text: 'Minesweeper', image: minesweeper, id: 10},
+  ]
+
+  const mouseOn = (e) => {
+    e.currentTarget.childNodes[0].classList.add('show');
+  }
+
+  const mouseGone = (e) => {
+    e.currentTarget.childNodes[0].classList.remove('show');
+  }
+
   return(
 
-    <Menu className={displayStatus}>
+    <Menu className={`${displayStatus} ${props.className}`}>
       {props.menuItems.map(item => {
+
+        if(item.id === 0) {
+          return (
+            <MenuItem key={item.id} onMouseOut={mouseGone} onMouseOver={mouseOn}>
+              <StartMenu className='mini' menuStatus={props.menuStatus} menuItems={miniMenu}/>
+              <img src={item.image} alt='menu icon' />
+              <p>{item.text}</p>
+            </MenuItem>
+          );
+        }
+
         return (
           <MenuItem key={item.id}>
             <img src={item.image} alt='menu icon' />
             <p>{item.text}</p>
           </MenuItem>
         );
+
       })}
     </Menu>
   );
