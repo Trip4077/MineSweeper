8b4ade9adeff1238f40b1ad9bf07979d6e5ed7b6
Trip1701 <bj2336@email.vccs.edu>
2019-01-18 23:33:23 -0500

    TimeBox styled, Updating TimeStamp implemented

--- src/components/TaskBar/StartBtn.js
+++ src/components/TaskBar/StartBtn.js
@@ -5,8 +5,8 @@ import icon from '../../images/start-icon.png';
 const StartBtn = props => {
   const Start = styled.button`
     height: 80%;
-    width: 5%;
-    border: 1px outset;
+    width: 5.5%;
+    border: 2.5px outset;
     border-color: white rgba(0, 0, 0, 0.6) rgba(0, 0, 0, 0.7) white;
     background-color: #c0c0c0;
     position: relative;
@@ -15,7 +15,7 @@ const StartBtn = props => {
     justify-content: space-around;
     align-items: center;
 
-    font-size: 1.6rem;
+    font-size: 1.8rem;
     letter-spacing: 1.5px;
 
     &:before {
@@ -41,10 +41,6 @@ const StartBtn = props => {
       height: 25px;
       width: 25px;
     }
-  `;
-
-  const Time = styled.p`
-
   `;
 
   return(
--- src/components/TaskBar/Time.js
+++ src/components/TaskBar/Time.js
@@ -1,12 +1,74 @@
 import React from 'react';
 import styled from 'styled-components';
+import aol from '../../images/AOL.png';
 
-const Time = props => {
-  return(
-    <div>
-      test
-    </div>
-  );
+const TimeBox = styled.div`
+  border: 2px inset #8b8b8b;
+  background-color: #b5abab;
+  height: 80%;
+  width: 7%;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  font-size: 2rem;
+
+  img {
+    cursor: pointer;
+  }
+`;
+
+class Time extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      time: ''
+    }
+  }
+
+  getTime = () => {
+    const today = new Date();
+    const hours = today.getHours()
+    let minutes = today.getMinutes();
+    let dayPart = 'AM';
+
+    if (minutes.toString().length === 1) {
+      minutes = "0" + minutes;
+    }
+
+    if (hours > 11) {
+      dayPart = 'PM';
+    }
+
+    const current =   `${hours}:${minutes} ${dayPart}`;
+
+    this.setState({
+      time: current
+    })
+  }
+
+  componentDidMount() {
+    if(this.state.time === '') {
+      this.getTime();
+    }
+
+    this.interval = setInterval(this.getTime, 10000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return(
+      <TimeBox>
+        <img src={aol} alt="AOL icon - 95" />
+        {this.state.time}
+      </TimeBox>
+    );
+  }
 }
 
 export default Time;
