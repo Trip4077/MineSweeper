fad58ac927ef973020800129944b6ea1283324c6
Trip1701 <bj2336@email.vccs.edu>
2019-01-20 09:25:34 -0500

    tile mine contact count function implemented

--- src/components/Minesweeper/TileContainer.js
+++ src/components/Minesweeper/TileContainer.js
@@ -8,12 +8,47 @@ class TileContainer extends React.Component {
     this.state = {
       tyleType: this.props.tile,
       clicked: false,
-      flagged: false
+      flagged: false,
+      mineContact: 0,
     }
   }
 
+  mineCheck = () => {
+    const origin = this.props.id;
+    const board = this.props.map;
+    const neighbors = [];
+
+    neighbors.push(board[origin + 8]);
+    neighbors.push(board[origin - 8]);
+
+    if(!origin === 0 || !(origin % 8 === 0)) {
+      neighbors.push(board[origin - 9]);
+      neighbors.push(board[origin - 1]);
+      neighbors.push(board[origin + 7]);
+    }
+
+    if(!origin === 7 || !(origin % 8 === 7)) {
+      neighbors.push(board[origin + 9]);
+      neighbors.push(board[origin - 7]);
+      neighbors.push(board[origin + 1]);
+
+    } else if(origin !== 63) {
+      neighbors.push(board[origin - 7]);
+    }
+
+    const mineCount = neighbors.filter(val => val === 1);
+
+    this.setState({
+      mineContact: mineCount.length
+    })
+  }
+
+  componentDidMount() {
+    this.mineCheck();
+  }
+
   render() {
-    return <Tile></Tile>
+    return <Tile>{this.state.mineContact}</Tile>
   }
 }
 
